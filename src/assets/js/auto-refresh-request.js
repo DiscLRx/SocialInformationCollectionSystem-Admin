10afc2727854fc221e797706e0f9b7f35a9a5b3b
DiscLR <rmfield@qq.com>
2023-07-03 19:15:37 +0800

    优化发生请求权限异常时的处理方式

--- src/assets/js/auto-refresh-request.js
+++ src/assets/js/auto-refresh-request.js
@@ -4,17 +4,25 @@ import router from "@/router";
 let count = 1;
 export default async function request(config) {
     return await axios(config).then((res)=>{
-        if (res.data.code===13){
-            if (count >= 5) {
+        let code = res.data.code
+        switch (code) {
+            case 0:
+                return res
+            case 11:
                 localStorage.removeItem('token')
                 router.push('/sign-in')
-            }
-            let token = res.data.data.token;
-            localStorage.setItem('token', token)
-            config['headers']['Token'] = token
-            return request(config)
-        }else {
-            return res
+                break
+            case 13:
+                if (count >= 5) {
+                    localStorage.removeItem('token')
+                    router.push('/sign-in')
+                }
+                let token = res.data.data.token;
+                localStorage.setItem('token', token)
+                config['headers']['Token'] = token
+                return request(config)
+            default:
+                return res
         }
     })
 }
